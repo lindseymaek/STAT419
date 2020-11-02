81e25cdb62c12a944409fe9984333da3142de4cf
Lindsey Kornowske <lindseykornowske@Lindseys-MacBook-Pro-2.local>
2020-11-02 08:38:19 -0800

    measure function

--- functions/functions-project-measure.R
+++ functions/functions-project-measure.R
@@ -81,14 +81,6 @@ prepareMeasureData <- function(measure) {
   measure = measure[complete.cases(measure$height.NA),];
   
   
-  #Did this to collapse the data, but then realized I was interested in differences between left and right
-  #collapseCols = c("hand.length.left", "measure$hand.length.right", "hand.width.left", "hand.width.right", "floor.armpit.left",
-                   "floor.armpit.right", "hand.elbow.left", "hand.elbow.right", "elbow.armpit.left", "elbow.armpit.right",
-                   "floor.kneepit.left", "floor.kneepit.right", "floor.hip.left", "floor.hip.right", "arm.reach.left", "arm.reach.right", "foot.length.left", "foot.length.right", 
-                   "units")
-  
-  #measure = measure[,-which(names(measure) %in% collapseCols)];
-  
   measure$ape.index = measure$arm.span.NA - measure$height.NA;
   
   
